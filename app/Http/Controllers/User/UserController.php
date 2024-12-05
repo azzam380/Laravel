711d693eb0885ab275f7369f87093a5b26fa9b37
Muahmmad Azzam <muh.azam0509@gmail.com>
2024-12-05 14:23:23 +0700

    first commit

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Masyarakat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,10 +15,33 @@ class UserController extends Controller
         return view('user.landing');
     }
 
+    public function login (Request $request)
+    {
+        $username = Masyarakat::where('username', $request->username)->first();
+
+        if (!$username) {
+            return redirect()->back()->with(['pesan'=> 'Username Not Found']);
+        }
+        // tanda '!' berarti tidak match
+
+        $password = Hash::check($request->password, $username->username);
+
+        if (!$password) {
+            return redirect()->back()->with(['pesan'=> 'Wrong Password']);
+        }
+
+        if (Auth::guard('Masyarakat')->attempt(['username'=> $request->username, 'password' => $request->password])) {
+            return redirect()->back();
+        } else {
+            return redirect()->with(['pesan', 'Account Not Registered']);
+        }
+    }
+
     public function formRegister()
-{
-    return view('user.register');
-}
+    {
+        return view('user.register');
+    }
+   
 }
 
 
--- resources/views/user/landing.blade.php
+++ resources/views/user/landing.blade.php
@@ -1,56 +1,63 @@
 @extends('layouts.user')
 
-<!-- pembuka head -->
+{{-- Pembuka Head --}}
 @section('css')
-<link rel="stylesheet" href="{{asset('css/landing.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
 @endsection
-<!-- asset itu mendefinisikan folder public -->
 
+    {{-- Asset itu mendefinisikan folder public --}}
+    {{-- yield diambil dengan menggunakan section --}}
+    {{-- extends itu mengambil semuanya --}}
 
-@section('title', 'Rehub - E-Report hub')
-<!-- penutup head -->
+{{-- Asset itu mendefinisikan folder public --}}
+@section('title', 'Rehub - E-Report Hub')
+{{-- Penutup Head --}}
 
-
-
-<!-- pembuka body -->
+{{-- Pembuka Body --}}
 @section('content')
-<!-- section header -->
- <section class="header">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
-        <div class="container">
-            <div class="container-fluid">
-                <a href="" class="navbar-brand">
-                    <h4 class="semi-bold mb-0 text-white">REHUB</h4>
-                    <p class="italic mt-0 text-white">E-REPORT Hub</p>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" 
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+    {{-- Section Header --}}
 
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    @if(Auth::guard('masyarakat')->check())
-                    <ul class="navbar-nav text-center ml-auto">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link ml-auto text-white">Report</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link ml-auto text-white"
-                             style="text-decoration: underline">{{Auth::guard('masyarakat')->user()->name}}</a>
-                        </li>
-                    </ul>
-@else
-                    <ul class="navbar-nav text-center ml-auto">
-                        <li class="nav-item">
-                            <button class="btn text-white" class="btn btn-warning" type="button"  data-toggle="modal" data-target="#loginModal">
-                            Login
-                            </button>
-                        </li>
-                        <li class="navbar-item">
-                            <a href="{{route('ereporthub.formRegister')}}" class="btn btn-outline-purple">Register</a>
-                        </li>
-                    </ul>
-                    @endauth
+    <section class="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+            <div class="container">
+                <div class="container-fluid">
+                    {{-- Perbedaan container dan coiner fluid. Container fluid menyesuaikan layaknya air
+                    sedangkan continer itu tetap --}}
+                    <a href="#" class="navbar-brand">
+                        <h4 class="semi-bold mb-0 text-white">REHUB</h4>
+                        <p class="italic mt-0 text-white">E-Report Hub</p>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        @if (Auth::guard('masyarakat')->check())
+
+                            <ul class="navbar-nav text-center ml-auto">
+                                <li class="nav-item">
+                                    <a href="{{ Route('ereporthub.report') }}" class="nav-link ml-auto text-white">Report</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ Route('ereporthub.logout') }}" class="nav-link ml-auto text-white"
+                                        style="text-decoration: underline">{{ Auth::guard('masyarakat')->user()->nama }}</a>
+                                </li>
+                            </ul>
+                        @else
+                            <ul class="navbar-nav text-center ml-auto">
+                                <li class="nav-item">
+                                    <button class="btn text-white" type="button" class="btn btn-warning"
+                                        data-toggle="modal" data-target="#loginModal">
+                                        {{-- btn-primary=biru, btn-danger=merah, btn-warning=kuning --}}
+                                        Login
+                                    </button>
+                                </li>
+
+                                <li class="navbar-item">
+                                    <a href="{{ Route('ereporthub.formRegister') }}" class="btn btn-outline-purple">Register</a>
+                                </li>
+                            </ul>
+                        @endauth
                 </div>
             </div>
         </div>
@@ -59,50 +66,50 @@
     <div class="text-center">
         <h2 class="medium text-white mt-3">Report Services</h2>
         <p class="italic text-white mb-5">Tell Us About Your Report</p>
+        {{-- mb itu margin bottom dan mt itu margin top --}}
     </div>
-    
+
     <div class="wave wave1"></div>
     <div class="wave wave2"></div>
     <div class="wave wave3"></div>
     <div class="wave wave4"></div>
- </section>
+</section>
 
- <!-- section card report -->
+{{-- Section Card Report --}}
 
-  <div class="row justify-content-center">
+<div class="row justify-content-center">
     <div class="col-lg-6 col-10 col">
+        {{-- lg=large, col=column --}}
         <div class="content shadow">
             @if ($errors->any())
-            @foreach ($errors->all() as $error )
-                <div class="alert alert-danger">{{$error}}</div>
-            @endforeach 
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">{{ $error }}</div>
+                @endforeach
             @endif
 
-            @if(Session::has('pengaduan'))
+            @if (Session::has('pengaduan'))
                 <div class="alert alert-{{ Session::get('type') }}">{{ Session::get('pengaduan') }}</div>
             @endif
-            
-             <div class="card mb-3">Create Yout Report Here</div>
+
+            <div class="card mb-3">Create Your Report Here</div>
             <form action="#" method="POST">
-                 @csrf
+                @csrf
                 <div class="form-group">
-                  <textarea name="isi_laporan" placeholder="Write Your Report Here" class="form-control" rows="4">
-                     {{ old('isi_laporan')}}
-                  </textarea>
+                    <textarea name="isi_laporan" placeholder="Write Your Report Here" class="form-control" rows="4">
+                        {{ old('isi_laporan') }}
+                    </textarea>
                 </div>
 
                 <div class="form-group">
-                  <input type="file" name="foto" class="form-control">
+                    <input type="file" name="foto" class="form-control">
                 </div>
-               <button type="Submit" class="btn btn-custom mt-2">Send</button>
+                <button type="submit" class="btn btn-custom mt-2">Send</button>
             </form>
-
         </div>
     </div>
-  </div>
+</div>
 
 {{-- Hitung Pengaduan --}}
-
 
 <div class="pengaduan mt-5">
     <div class="bg-purple">
@@ -114,55 +121,51 @@
 </div>
 
 {{-- Footer --}}
-
 <div class="mt-5">
     <hr>
     <div class="text-center">
-        <p class="italic text-secondary">© 2024 Azzam | All rights reserved</p>
+        <p class="italic text-secondary">© 2024 Zaam • All rights reserved</p>
     </div>
 </div>
-  
-<!-- penutup body -->
-
+{{-- Penutup Body --}}
 
 {{-- Modal --}}
-
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-   <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-body">
-            <h3 class="mt-3">Login First</h3>
-            <p>Please Login With Registred Account.</p>
-            <form action="#" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="usernamd">Username</label>
-                    <input type="text" name="username" id="username" class="form-control">
-                   </div>
-
-                   <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" class="form-control">
-                   </div>
-                   <button type="submit" class="btn btn-purple text-white mt-3" style="width: 100%">Login</button>
-            </form>
-            @if(Session::has('pesan'))
-                  <div class="alert alert-danger mt-2">
-                    {{Session::get('pesan')}}
-                  </div>
-            @endif
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h3 class="mt-3">Login First</h3>
+                <p>Please Login With Registered Account.</p>
+                <form action="{{ Route('ereporthub.login')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-purple text-white mt-3" style="width: 100%">Login</button>
+                </form>
+                @if (Session::has('pesan'))
+                    <div class="alert alert-danger mt-2">
+                        {{ Session::get('pesan') }}
+                    </div>
+                @endif
+            </div>
         </div>
-      </div>
-   </div>
+    </div>
 </div>
 @endsection
 
 
+
 @section('js')
-@if(Session::has('pesan'))
-<script>
-    $('#loginModal').modal('show')
-</script>
+@if (Session::has('pesan'))
+    <script>
+        $('#loginModal').modal('show');
+    </script>
 @endif
 @endsection
 
